14f0f5cba5991f8678f7744216c7eabd10d2ff40
jonesmttw <jonesmttw@gmail.com>
2014-09-26 16:00:18 -0500

    tmpl.js
    updating

--- js/tmpl.js
+++ js/tmpl.js
@@ -1,5 +1,3 @@
-// will replace occurences of <% ### %> within your string
-
 function tmpl(str, args) {
     for (var i = 0; i < args.length; i++) {
         str = str.replace(new RegExp('<%' + i.toString() + '%>', 'g'), args[i]);
